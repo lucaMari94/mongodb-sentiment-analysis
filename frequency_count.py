4f3bfda8d8f36ff59847c355f048a55875ec5d02
Borla <borla95@gmail.com>
2020-04-15 17:20:53 +0200

    finish write frequency into file

--- frequency_count.py
+++ frequency_count.py
@@ -25,10 +25,28 @@ def get_result_count_dict(result_count_dict):
 
 
 def get_count_word_total(table_name):
+
+    if table_name == 'anger':
+        query = "SELECT SUM(frequency) from " + table_name + " where (emo_sn = 1 or nrc = 1 or sentisense = 1)"
+    elif table_name == 'anticipation':
+        query = "SELECT SUM(frequency) from " + table_name + " where (nrc = 1 or sentisense = 1)"
+    elif table_name == 'disgust':
+        query = "SELECT SUM(frequency) from " + table_name + " where (nrc = 1 or sentisense_disgust = 1 " \
+                                                             "or sentisense_hate = 1)"
+    elif table_name == 'fear':
+        query = "SELECT SUM(frequency) from " + table_name + " where (nrc = 1 or sentisense = 1)"
+    elif table_name == 'joy':
+        query = "SELECT SUM(frequency) from " + table_name + " where (emo_sn = 1 or nrc = 1 or sentisense = 1)"
+    elif table_name == 'sadness':
+        query = "SELECT SUM(frequency) from " + table_name + " where (nrc = 1 or sentisense = 1)"
+    elif table_name == 'surprise':
+        query = "SELECT SUM(frequency) from " + table_name + " where (nrc = 1 or sentisense = 1)"
+    elif table_name == 'trust':
+        query = "SELECT SUM(frequency) from " + table_name + " where (nrc = 1)"
+
     db = connect_to_db()
     try:
         mycursor = db.cursor()
-        query = "SELECT SUM(frequency) from "+table_name+" where (emo_sn = 1 or nrc = 1 or sentisense = 1)"
         mycursor.execute(query)
         return mycursor.fetchone()[0]
     except mysql.connector.Error as e:
@@ -38,15 +56,15 @@ def get_count_word_total(table_name):
 def perc_lexical_into_twitter_msg(emotion):
 
     count_total_emotion = get_count_word_total(emotion)
-
     emotion_global_dict_count = get_result_count_dict(emotion+'_global_dict_count')
     total_word_dict_twit = sum(emotion_global_dict_count.values())
 
-    perc_emotion = count_total_emotion/ total_word_dict_twit * 100
+    perc_emotion = count_total_emotion/total_word_dict_twit * 100
 
     return f"{perc_emotion:.2f}"
 
-arr_emotions = ["anger","anticipation","disgust","fear","joy","sadness","surprise","trust"]
+
+arr_emotions = ["anger", "anticipation", "disgust", "fear", "joy", "sadness", "surprise", "trust"]
 
 file = open("result_frequency/frequency.txt", "a", encoding='utf-8')
 
