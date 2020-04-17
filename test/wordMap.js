c1204b37f4193844cac20239d1d5716199a18ffb
luca <luca.marignati@edu.unito.it>
2020-04-17 17:00:11 +0200

    mongodb test

---
+++ test/wordMap.js
@@ -0,0 +1,13 @@
+function wordMap(){
+    // find words in the document text
+    var words = this.text.match(/\w+/g);
+
+    if(words == null){
+        return
+    }
+
+    for(var i = 0; i < words.length; i++){
+        //emit every word, with count of one
+        emit(words[i], {count:1})
+    }
+}
---
+++ test/wordsMapReduce.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+from bson.code import Code
+
+# client
+connection = MongoClient('localhost', 27017)
+db = connection.test
+
+# insert data
+# myfile = open("words.txt", "rt", encoding='utf-8')
+myfile = open("../twitter_message/dataset_dt_" + "anger" + "_60k.txt", "rt", encoding='utf-8')
+lines = myfile.read()
+myfile.close()
+
+[db.texts.insert_one({'text': line}) for line in lines.split()]
+
+
+# load map and reduce functions
+map = Code(open('wordMap.js', 'r').read())
+reduce = Code(open('wordReduce.js', 'r').read())
+
+# run the map-reduce query
+results = db.texts.map_reduce(map, reduce, "output")
+
+for result in results.find():
+    print(result['_id'], result['value']['count'])
+
